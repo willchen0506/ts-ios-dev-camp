41a8bf124706a6111bf38dea85102506d39fef8e
Will Chen <willchen0506@gmail.com>
2013-07-13 12:11:12 -0700

    initial hello world commit

---
+++ HelloWorld/HelloWorld/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  HelloWorld
+//
+//  Created by Will Chen on 7/13/13.
+//  Copyright (c) 2013 TapSense. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ HelloWorld/HelloWorld/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  HelloWorld
+//
+//  Created by Will Chen on 7/13/13.
+//  Copyright (c) 2013 TapSense. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
