16a981a9dc0b3c8a2bb593ab416e249dbc053071
Will Chen <willchen0506@gmail.com>
2013-09-26 21:38:44 -0700

    iPerv initial commit.

---
+++ iPerv/iPerv/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  iPerv
+//
+//  Created by Will Chen on 9/26/13.
+//  Copyright (c) 2013 TapSense. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ iPerv/iPerv/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  iPerv
+//
+//  Created by Will Chen on 9/26/13.
+//  Copyright (c) 2013 TapSense. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
